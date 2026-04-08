cc560226d5b5532b558f890be37292f7c87862f7
Mac jenkins <devops@datazoom.io>
2026-04-07 21:14:45 -0700

    Release 1.1.3: Updated repository with latest Package.swift file.

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "MediaTailorSDK",
-            url: "https://github.com/datazoomer/apple_mediatailorsdk/releases/download/1.1.2/MediaTailorSDK.xcframework.zip",
-            checksum: "cf2a646abbf6a292d2df711e99106183deff73a8ad5a103f90b05aa851d8939b")
+            url: "https://github.com/datazoomer/apple_mediatailorsdk/releases/download/1.1.3/MediaTailorSDK.xcframework.zip",
+            checksum: "e5634770a5edfe660c19f7f4ec9d37365ae644cfb940413f9153a8aca4f85202")
     ],
     swiftLanguageVersions: [.v5]
 )
